121a0fc16e7922ede18a64255fd3d305d3f12d73
Denesh11 <denesh@zionstar.in>
2025-03-20 14:37:46 +0530

    change name space

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.helpalife_mobile"
+    namespace = "in.helpalife"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
--- android/app/src/main/kotlin/in/helpalife/MainActivity.kt
+++ android/app/src/main/kotlin/in/helpalife/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.helpalife_mobile
+package `in`.helpalife
 
 import io.flutter.embedding.android.FlutterActivity
 
